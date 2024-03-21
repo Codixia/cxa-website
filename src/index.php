97f7fccf38e3b5139011f0e164b571adfaa19776
Nhexen <riwn.jpn@gmail.com>
2024-03-21 16:49:59 +0100

    Adding sass and try to run node lmao

---
+++ src/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+    <html lang='en'>
+    <head>
+        <meta charset='UTF-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>Codixia</title>
+        <link rel='stylesheet' href='../public/main.css'>
+    </head>
+    <body>
+        <header>
+            <h1>Codixia</h1>
+            <nav>
+                <ul>
+                    <li><a href='app.php'>Home</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <h2>Welcome to Codixia</h2>
+            <p>Under Construction</p>
+        </main>
+        <footer>
+            <p>&copy; 2024 - Codixia</p>
+        </footer>
+    </body>
+    </html>
